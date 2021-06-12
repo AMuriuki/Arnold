4858a1c595350184a963c6ab69eaa3b98191b40b
Arnold Nderitu <arnoldnderitu@gmail.com>
2021-06-12 19:53:15 +0300

    slider images

--- kajiadoacres/home/models.py
+++ kajiadoacres/home/models.py
@@ -1,15 +1,35 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
 
 
 class HomePage(Page):
     header_one = models.CharField(max_length=250, default='Find Land for Sale')
-    header_two = models.CharField(max_length=250, default='Your trusted county experts in pre-verified properties.')
+    header_two = models.CharField(
+        max_length=250, default='Your trusted county experts in pre-verified properties.')
 
     content_panels = Page.content_panels + [
         FieldPanel('header_one'),
         FieldPanel('header_two'),
+        InlinePanel('slider_images', label="Slider images"),
+    ]
+
+
+class HomePageGalleryImage(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE,
+                       related_name='slider_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
     ]
