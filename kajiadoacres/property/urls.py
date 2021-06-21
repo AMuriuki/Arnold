75516eff28a31978883d79e51f0686ea8c4822a8
Arnold Nderitu <arnoldnderitu@gmail.com>
2021-06-21 20:38:43 +0300

    property details

--- kajiadoacres/property/urls.py
+++ kajiadoacres/property/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PropertyView.as_view(), name='property')
+    path('', views.PropertyView.as_view(), name='property'),
+    path('details/<int:pk>', views.PropertyDetailsView.as_view(),
+         name='property-details')
 ]
--- kajiadoacres/property/views.py
+++ kajiadoacres/property/views.py
@@ -3,12 +3,18 @@ from django.http import HttpResponse
 from django.views.generic import (View, TemplateView, ListView,
                                   DetailView, CreateView,
                                   UpdateView, DeleteView)
-
-
-def property(request):
-    print("!!!!!!!!!!!!!!!!!!!!")
-    return HttpResponse("Working")
+from .models import *
 
 
 class PropertyView(TemplateView):
     template_name = 'property/property.html'
+
+
+class PropertyDetailsView(DetailView):
+    model = Property
+    template_name = 'property/property.html'
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(PropertyDetailsView, self).get_context_data(*args, **kwargs)
+        ctx['']
+        
